98df9a1e6b2186218533b6bcd9f6ccdae0d3b6f7
Jae Jackson <takeoverhen276@gmail.com>
2025-08-29 11:48:21 -0400

    Add files via upload

---
+++ SimpleHTTPServer.java
@@ -0,0 +1,23 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class SimpleHTTPServer {
+
+    public static void main(String[] args) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080........");
+
+        while (true){
+           try (Socket clientSocket = server.accept()){
+                Date today = new Date();
+
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+
+                clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+           }
+
+
+        }
+    }
+}
